794e2ac17f9a128c8fd2a7ebf1a8d99f29b353f4
girvel <widauka@ya.ru>
2024-01-14 03:12:33 +0500

    Move common zshrc elements here

---
+++ zshrc-common.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env zsh
+
+# CROSS-PLATFORM SHIT #
+if [ -n "$WSL_DISTRO_NAME" ]; then
+    alias g="git.exe"
+    alias c='cargo.exe'
+else
+    alias g="git"
+    alias c="cargo"
+fi
+
+# BASE #
+alias A='sudo aptitude'
+alias e='$EDITOR'
+alias E='sudo $EDITOR'
+alias l='ls -lah --group-directories-first'
+
+# EDITOR #
+git config --global core.editor "$EDITOR"
+
+# GIT #
+alias gac='g add `g rev-parse --show-toplevel` && git commit'
+alias gacp='gac && g push'
+alias gst='g status'
+alias gd='g diff'
+alias glog='g log --oneline --graph'
+alias gpull='g pull'
+
+# EDITING COMMANDS & CONFIGURATION #
+alias erc='e ~/.zshrc; source ~/.zshrc'
+function ebin() {
+    e ~/.local/bin/"$1"
+    chmod +x ~/.local/bin/"$1"
+}
+
